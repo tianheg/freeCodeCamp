c1d5f1b36f9901e5e5323ed12500528d9be72183
tianheg <yidajiabei@gmail.com>
2023-03-20 15:38:21 +0800

    replace next-pwa with @ducanh2912/next-pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const runtimeCaching = require('next-pwa/cache')
-const withPWA = require('next-pwa')({
+
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-  runtimeCaching,
 })
 
 const nextConfig = withPWA({
