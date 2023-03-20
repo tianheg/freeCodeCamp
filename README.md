064e50fc6c8216b6c27963ecfe58a068f7453382
tianheg <yidajiabei@gmail.com>
2023-03-20 15:11:07 +0800

    test work or not

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # freeCodeCamp projects
 
 > https://www.freecodecamp.org/learn
+
+## References
+
+- https://github.com/vercel/next.js/tree/canary/examples/progressive-web-app
+- 
--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
 })
 
 const nextConfig = withPWA({
   experimental: {
     appDir: true,
-  },
+  }
 })
 
 module.exports = nextConfig
