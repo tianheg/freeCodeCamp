b70474d2956a4544248a3592a6f8995daad4040f
tianheg <yidajiabei@gmail.com>
2023-03-20 15:28:45 +0800

    modify PWA config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require('next-pwa/cache')
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development'
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
 })
 
 const nextConfig = withPWA({
   experimental: {
     appDir: true,
-  }
+  },
+  reactStrictMode: false,
 })
 
 module.exports = nextConfig
