dcfebe1bfed5b73ecf5977ba22a2a48ca5fb5246
tianheg <yidajiabei@gmail.com>
2023-03-20 14:57:56 +0800

    support PWA

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,15 +3,27 @@ import './globals.scss'
 export const metadata = {
   title: 'freeCodeCamp projects',
   description: 'Created by tianheg',
+  themeColor: 'white',
+  manifest: '/manifest.json',
+  icons: {
+    icon: [
+      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'images/png' },
+      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'images/png' },
+    ],
+    shortcut: '/favicon.ico',
+    apple: '/icons/apple-touch-icon.png',
+    other: {
+      rel: 'mask-icon',
+      url: '/icons/safari-pinned-tab.svg',
+      color: '#5bbad5',
+    },
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
